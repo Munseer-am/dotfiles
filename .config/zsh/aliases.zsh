4e686bbb47712fecaac679925d60ca5fea2caa1f
Munseer-am <munnamunseer7@gmail.com>
2024-10-03 20:02:34 +0530

    Installed new rice

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,16 +2,17 @@
 
 # Basic aliases
 # alias rm='rm -rIv'   # Recursive, interactive, and verbose `rm`
-# alias cp='cp -rv'   # Recursive and verbose `cp`
-# alias mv='mv -v'    # Verbose `mv`
-# alias mkdir='mkdir -p'  # recursive mkdir
+ alias cp='cp -rv'   # Recursive and verbose `cp`
+ alias mv='mv -v'    # Verbose `mv`
+ alias mkdir='mkdir -p'  # recursive mkdir
 alias grep='rg' # use ripgrep
 alias cat='bat'
+alias battery='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage'
 
 # `eza` aliases for more informative and colored output
 alias la='eza --icons -a --group-directories-first'  # List all files and directories, including hidden ones
 alias ll='eza --icons -lah --group-directories-first'  # List in long format with permissions and human-readable sizes
-alias ls='eza --icons --group-directories-first'  # List files and directories
+alias ls='eza -l --icons --group-directories-first'  # List files and directories
 
 # `eza` alias for displaying directory trees
 alias tree='eza --icons --tree'
@@ -32,3 +33,5 @@ alias rs='curl --data-binary @- https://paste.rs | wl-copy'  # Share output to a
 alias vim='$EDITOR'
 alias vi='$EDITOR'
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
+alias sus='systemctl suspend'
+alias update='sudo pacman -Syyu'
